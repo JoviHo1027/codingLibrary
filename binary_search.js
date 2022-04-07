9c200ec82eacb3927b34b41ab687a4ff70de0e0a
JoviHo <cyw05095@gmail.com>
2022-04-07 13:42:15 +0800

    Binary search and Priority_queue

---
+++ binary_search.js
@@ -0,0 +1,20 @@
+const binarySearch = (sortedArray, key) => {
+    let start = 0;
+    let end = sortedArray.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+
+        if (sortedArray[middle] === key) {
+            return middle;
+        } else if (sortedArray[middle] < key) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+
+    return -1;
+}
+
+console.log(binarySearch([1, 4, 6, 8, 11], 66));
